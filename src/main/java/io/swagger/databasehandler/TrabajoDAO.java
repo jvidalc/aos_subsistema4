47c7e1d034b0f5b32f1b23d2e3da1e1a4fdfc971
Jaime Vidal <jaime.vicol97@gmail.com>
2021-06-10 19:18:02 +0200

    Corregido TrabajoDAO -> obtener()

--- src/main/java/io/swagger/databasehandler/TrabajoDAO.java
+++ src/main/java/io/swagger/databasehandler/TrabajoDAO.java
@@ -44,10 +44,10 @@ public class TrabajoDAO {
         try (PreparedStatement ps = con.prepareStatement("SELECT * FROM trabajos WHERE id = ?;")) {
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
-            Trabajo trab = new Notificacion();
+            Trabajo trab = new Trabajo();
             trab.setTrabajoId(rs.getInt("id"));
-            trab.setNombreTrabajo(rs.getIn("nombreTrabajo"))
-            trab.setEstadoTrabajo(rs.getString("estado"));
+            trab.setNombreTrabajo(rs.getString("nombreTrabajo"));
+            trab.setEstadoTrabajo(Trabajo.EstadoTrabajoEnum.fromValue(rs.getString("estado")));
 
             return trab;
         } catch (Exception e) {
