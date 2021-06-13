10d9a740e7c8fbe158be36494195ec6c7a6e1ef8
Jaime Vidal <jaime.vicol97@gmail.com>
2021-06-13 10:23:54 +0200

    Vuelta al mocking

--- src/main/java/io/swagger/api/NotificacionApiController.java
+++ src/main/java/io/swagger/api/NotificacionApiController.java
@@ -1,42 +1,25 @@
 package io.swagger.api;
 
-import io.swagger.databasehandler.NotificacionDAO;
-import io.swagger.model.HTTPProblem;
 import io.swagger.model.InlineResponse200;
 import io.swagger.model.Notificacion;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.util.MultiValueMap;
 
 import javax.validation.constraints.*;
 import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.net.URI;
-import java.util.List;
-import java.util.Map;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-18T16:15:28.764Z[GMT]")
 @RestController
@@ -47,37 +30,24 @@ public class NotificacionApiController implements NotificacionApi {
     private final ObjectMapper objectMapper;
 
     private final HttpServletRequest request;
-    private final NotificacionDAO notificacionDAO;
 
     @org.springframework.beans.factory.annotation.Autowired
     public NotificacionApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
-        notificacionDAO = new NotificacionDAO();
     }
 
     public ResponseEntity<InlineResponse200> s4NotificacionArrayGet() {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
-                //Coger dato de BD
-                List<Notificacion> lista_notificaciones = notificacionDAO.obtenerTodos();
-                //Convertirlo a string con formato JSON
-                String json = "{\n  \"notificaciones\" : [";
-                for(Notificacion noti : lista_notificaciones){
-                    json += "{\n    \"notificacionId\" : " + noti.getNotificacionId() + ",\n    \"clienteId\" : " +  noti.getClienteId() + ",\n    \"fechaNotificacion\" : \"" + noti.getFechaNotificacion().toString() + "\",\n    \"objetoTrabajo\" : {\n      \"Trabajo\" : {\n        \"trabajoId\" : " + noti.getTrabajo().getTrabajoId() + ",\n        \"nombreTrabajo\" : \"" + noti.getTrabajo().getNombreTrabajo() + " \",\n        \"estadoTrabajo\" : \"" + noti.getTrabajo().getEstadoTrabajo() + "\"\n      }\n    },\n    \"links\" : {\n      \"parent\" : {\n        \"href\" : \"/api/v1/notificacion\",\n        \"rel\" : \"lista_notificaciones crear_notificacion\"\n      },\n      \"self\" : {\n        \"href\" : \"/api/v1/notificacion/1234\",\n        \"rel\" : \"modificar_notificacion eliminar_notificacion\"\n      }\n    }\n  },";
-                }
-                StringBuffer aux = new StringBuffer(json);
-                aux.deleteCharAt(json.length()-1);
-                json = aux.toString();
-                json += "]\n}";
-                //retornarlo
-                return new ResponseEntity<InlineResponse200>(objectMapper.readValue(json, InlineResponse200.class), HttpStatus.OK);
+                return new ResponseEntity<InlineResponse200>(objectMapper.readValue("{\n  \"notificaciones\" : [ {\n    \"notificacionId\" : 1234,\n    \"clienteId\" : 5678,\n    \"fechaNotificacion\" : \"2021-04-06\",\n    \"objetoTrabajo\" : {\n      \"Trabajo\" : {\n        \"trabajoId\" : 91011,\n        \"nombreTrabajo\" : \"Revisión\",\n        \"estadoTrabajo\" : \"creado\"\n      }\n    },\n    \"links\" : {\n      \"parent\" : {\n        \"href\" : \"/api/v1/notificacion\",\n        \"rel\" : \"lista_notificaciones crear_notificacion\"\n      },\n      \"self\" : {\n        \"href\" : \"/api/v1/notificacion/1234\",\n        \"rel\" : \"modificar_notificacion eliminar_notificacion\"\n      }\n    }\n  }, {\n    \"notificacionId\" : 1234,\n    \"clienteId\" : 5678,\n    \"fechaNotificacion\" : \"2021-04-06\",\n    \"objetoTrabajo\" : {\n      \"Trabajo\" : {\n        \"trabajoId\" : 91011,\n        \"nombreTrabajo\" : \"Revisión\",\n        \"estadoTrabajo\" : \"creado\"\n      }\n    },\n    \"links\" : {\n      \"parent\" : {\n        \"href\" : \"/api/v1/notificacion\",\n        \"rel\" : \"lista_notificaciones crear_notificacion\"\n      },\n      \"self\" : {\n        \"href\" : \"/api/v1/notificacion/1234\",\n        \"rel\" : \"modificar_notificacion eliminar_notificacion\"\n      }\n    }\n  } ]\n}", InlineResponse200.class), HttpStatus.OK);
             } catch (IOException e) {
                 log.error("Couldn't serialize response for content type application/json", e);
                 return new ResponseEntity<InlineResponse200>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
+
         return new ResponseEntity<InlineResponse200>(HttpStatus.NOT_ACCEPTABLE);
     }
 
@@ -85,50 +55,49 @@ public class NotificacionApiController implements NotificacionApi {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             HttpHeaders responseHeaders = new HttpHeaders();
-            responseHeaders.add("Allow", "GET, OPTIONS");
+            responseHeaders.add("Allow", "GET, OPTIONS, POST");
             return new ResponseEntity<Void>(responseHeaders, HttpStatus.OK);
         }
         return new ResponseEntity<Void>(HttpStatus.NOT_ACCEPTABLE);
     }
 
     public ResponseEntity<Void> s4NotificacionDelete(@Pattern(regexp="^\\d+$") @Parameter(in = ParameterIn.PATH, description = "ID de una notificación", required=true, schema=@Schema()) @PathVariable("notificacionId") Integer notificacionId) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            notificacionDAO.eliminar(notificacionId);
-        }
-        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
     public ResponseEntity<Notificacion> s4NotificacionGet(@Pattern(regexp="^\\d+$") @Parameter(in = ParameterIn.PATH, description = "ID de una notificación", required=true, schema=@Schema()) @PathVariable("notificacionId") Integer notificacionId) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
-                Notificacion noti = notificacionDAO.obtener(notificacionId);
-                return new ResponseEntity<Notificacion>(objectMapper.readValue("{\n  \"notificacionId\" : " + noti.getNotificacionId() + ",\n  \"clienteId\" : " + noti.getClienteId() + ",\n  \"fechaNotificacion\" : " + noti.getFechaNotificacion() + ",\n  \"objetoTrabajo\" : {\n    \"Trabajo\" : {\n      \"trabajoId\" : " + noti.getTrabajo().getTrabajoId() + ",\n      \"nombreTrabajo\" : " + noti.getTrabajo().getNombreTrabajo() + ",\n      \"estadoTrabajo\" : " + noti.getTrabajo().getEstadoTrabajo() + "\n    }\n  },\n  \"links\" : {\n    \"parent\" : {\n      \"href\" : \"/api/v1/notificacion\",\n      \"rel\" : \"lista_notificaciones crear_notificacion\"\n    },\n    \"self\" : {\n      \"href\" : \"/api/v1/notificacion/1234\",\n      \"rel\" : \"modificar_notificacion eliminar_notificacion\"\n    }\n  }\n}", Notificacion.class), HttpStatus.NOT_IMPLEMENTED);
+                return new ResponseEntity<Notificacion>(objectMapper.readValue("{\n  \"notificacionId\" : 1234,\n  \"clienteId\" : 5678,\n  \"fechaNotificacion\" : \"2021-04-06\",\n  \"objetoTrabajo\" : {\n    \"Trabajo\" : {\n      \"trabajoId\" : 91011,\n      \"nombreTrabajo\" : \"Revisión\",\n      \"estadoTrabajo\" : \"creado\"\n    }\n  },\n  \"links\" : {\n    \"parent\" : {\n      \"href\" : \"/api/v1/notificacion\",\n      \"rel\" : \"lista_notificaciones crear_notificacion\"\n    },\n    \"self\" : {\n      \"href\" : \"/api/v1/notificacion/1234\",\n      \"rel\" : \"modificar_notificacion eliminar_notificacion\"\n    }\n  }\n}", Notificacion.class), HttpStatus.OK);
             } catch (IOException e) {
                 log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Notificacion>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<Notificacion>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
 
-        return new ResponseEntity<Notificacion>(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<Notificacion>(HttpStatus.NOT_ACCEPTABLE);
     }
 
     public ResponseEntity<Void> s4NotificacionOptions(@Pattern(regexp="^\\d+$") @Parameter(in = ParameterIn.PATH, description = "ID de una notificación", required=true, schema=@Schema()) @PathVariable("notificacionId") Integer notificacionId) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             HttpHeaders responseHeaders = new HttpHeaders();
-            responseHeaders.add("Allow", "GET, POST, PUT, OPTIONS, DELETE");
+            responseHeaders.add("Allow", "GET, PUT, DELETE, OPTIONS");
             return new ResponseEntity<Void>(responseHeaders, HttpStatus.OK);
         }
         return new ResponseEntity<Void>(HttpStatus.NOT_ACCEPTABLE);
     }
 
-    public ResponseEntity<Notificacion> s4NotificacionPost(@Parameter(in = ParameterIn.DEFAULT, description = "`Notificacion` data", required=true, schema=@Schema()) @Valid @RequestBody Notificacion notificacion) {
+    public ResponseEntity<Notificacion> s4NotificacionPost(@Parameter(in = ParameterIn.DEFAULT, description = "`Notificacion` data", required=true, schema=@Schema()) @Valid @RequestBody Object body) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
-            notificacionDAO.insertar(notificacion);
-            return new ResponseEntity<Notificacion>(HttpStatus.OK);
+            try {
+                return new ResponseEntity<Notificacion>(objectMapper.readValue("{\n  \"notificacionId\" : 1234,\n  \"clienteId\" : 5678,\n  \"fechaNotificacion\" : \"2021-04-06\",\n  \"objetoTrabajo\" : {\n    \"Trabajo\" : {\n      \"trabajoId\" : 91011,\n      \"nombreTrabajo\" : \"Revisión\",\n      \"estadoTrabajo\" : \"creado\"\n    }\n  },\n  \"links\" : {\n    \"parent\" : {\n      \"href\" : \"/api/v1/notificacion\",\n      \"rel\" : \"lista_notificaciones crear_notificacion\"\n    },\n    \"self\" : {\n      \"href\" : \"/api/v1/notificacion/1234\",\n      \"rel\" : \"modificar_notificacion eliminar_notificacion\"\n    }\n  }\n}", Notificacion.class), HttpStatus.CREATED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<Notificacion>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
         }
         return new ResponseEntity<Notificacion>(HttpStatus.NOT_ACCEPTABLE);
     }
@@ -137,14 +106,14 @@ public class NotificacionApiController implements NotificacionApi {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
-                return new ResponseEntity<Notificacion>(objectMapper.readValue("{\n  \"notificacionId\" : 1234,\n  \"clienteId\" : 5678,\n  \"fechaNotificacion\" : \"2021-04-06\",\n  \"objetoTrabajo\" : {\n    \"Trabajo\" : {\n      \"trabajoId\" : 91011,\n      \"nombreTrabajo\" : \"Revisión\",\n      \"estadoTrabajo\" : \"creado\"\n    }\n  },\n  \"links\" : {\n    \"parent\" : {\n      \"href\" : \"/api/v1/notificacion\",\n      \"rel\" : \"lista_notificaciones crear_notificacion\"\n    },\n    \"self\" : {\n      \"href\" : \"/api/v1/notificacion/1234\",\n      \"rel\" : \"modificar_notificacion eliminar_notificacion\"\n    }\n  }\n}", Notificacion.class), HttpStatus.NOT_IMPLEMENTED);
+                return new ResponseEntity<Notificacion>(objectMapper.readValue("{\n  \"notificacionId\" : 1234,\n  \"clienteId\" : 5678,\n  \"fechaNotificacion\" : \"2021-04-06\",\n  \"objetoTrabajo\" : {\n    \"Trabajo\" : {\n      \"trabajoId\" : 91011,\n      \"nombreTrabajo\" : \"Revisión\",\n      \"estadoTrabajo\" : \"creado\"\n    }\n  },\n  \"links\" : {\n    \"parent\" : {\n      \"href\" : \"/api/v1/notificacion\",\n      \"rel\" : \"lista_notificaciones crear_notificacion\"\n    },\n    \"self\" : {\n      \"href\" : \"/api/v1/notificacion/1234\",\n      \"rel\" : \"modificar_notificacion eliminar_notificacion\"\n    }\n  }\n}", Notificacion.class), HttpStatus.OK);
             } catch (IOException e) {
                 log.error("Couldn't serialize response for content type application/json", e);
                 return new ResponseEntity<Notificacion>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
 
-        return new ResponseEntity<Notificacion>(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<Notificacion>(HttpStatus.NOT_ACCEPTABLE);
     }
 
 }
